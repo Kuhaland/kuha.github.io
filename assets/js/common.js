51ad98a5675331ebce432ae38507b4d4a2e21f46
kuha <leehx78@gmail.com>
2022-07-03 18:04:38 +0900

    modified / added

--- assets/js/common.js
+++ assets/js/common.js
@@ -12,19 +12,19 @@ $(function () { // wait for document ready
     // define movement of panels
     var wipeAnimation = new TimelineMax()
         // animate to panel
-        .to("#slideContainer", 0.5, {z: -150, delay: 1})
+        .to("#slideContainer", 0.5, {z: -150, delay: 2})
         .to("#slideContainer", 1,   {x: "-20%"})
         .to("#slideContainer", 0.5, {z: 0})
 
-        .to("#slideContainer", 0.5, {z: -150, delay: 1})
+        .to("#slideContainer", 0.5, {z: -150, delay: 2})
         .to("#slideContainer", 1,   {x: "-40%"})
         .to("#slideContainer", 0.5, {z: 0})
 
-        .to("#slideContainer", 0.5, {z: -150, delay: 1})
+        .to("#slideContainer", 0.5, {z: -150, delay: 2})
         .to("#slideContainer", 1,   {x: "-60%"})
         .to("#slideContainer", 0.5, {z: 0})
 
-        .to("#slideContainer", 0.5, {z: -150, delay: 1})
+        .to("#slideContainer", 0.5, {z: -150, delay: 2})
         .to("#slideContainer", 1,   {x: "-80%"})
         .to("#slideContainer", 0.5, {z: 0});
 
