8618034184d3ad3cb4f3cd8f5fbc511338323e2d
kuha <leehx78@gmail.com>
2022-07-05 08:59:12 +0900

    modified / added

--- assets/js/common.js
+++ assets/js/common.js
@@ -29,8 +29,41 @@ $(function () { // wait for document ready
             .addTo(controller);
     }
 });
+
 $(document).ready(function(){
     $('.contact__button-item').each(function() {
         $(this).append('<span></span><span></span>')
     });
 });
+
+var modals = document.getElementsByClassName("modal");
+var btns = document.getElementsByClassName("content__inner-btn");
+var spanes = document.getElementsByClassName("modal__close");
+var funcs = [];
+
+function Modal(num) {
+    return function() {
+        btns[num].onclick =  function() {
+            modals[num].style.display = "block";
+            console.log(num);
+        };
+
+        spanes[num].onclick = function() {
+            modals[num].style.display = "none";
+        };
+    };
+}
+
+for(var i = 0; i < btns.length; i++) {
+    funcs[i] = Modal(i);
+}
+
+for(var j = 0; j < btns.length; j++) {
+    funcs[j]();
+}
+
+window.onclick = function(event) {
+    if (event.target.className == "modal") {
+        event.target.style.display = "none";
+    }
+};
