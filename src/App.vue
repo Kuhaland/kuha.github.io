bb63652074f66202e3246b571488001d037b065d
leehx78 <leehx78@smartscore.kr>
2024-09-04 10:07:45 +0900

    fixed

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+
 
 <template>
   <div>
@@ -14,5 +12,8 @@ import HelloWorld from './components/HelloWorld.vue'
   <HelloWorld msg="Vite + Vue" />
 </template>
 
+<!--<script setup>-->
+<!--import HelloWorld from './components/HelloWorld.vue'-->
+<!--</script>-->
 <style lang="scss" scoped>
 </style>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
-export default {
-  root: "src",
-  build: {
-    outDir: "../public",
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/',
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
-};
+  server: {
+    port: 3000
+  }
+})
